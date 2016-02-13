f67b9b3a330965a193a90a4c61ba82767f4fc672
EbenZhang <zhangyingneng@gmail.com>
2016-02-13 14:30:23 +0800

    Add plugin name to the interface

--- DBRestorer.Plugin.Interface/IPostDbRestore.cs
+++ DBRestorer.Plugin.Interface/IPostDbRestore.cs
@@ -5,5 +5,6 @@ namespace DBRestorer.Plugin.Interface
     public interface IPostDbRestore 
     {
         void OnDBRestored(Window parentWnd, string sqlInstName, string dbName);
+        string PluginName { get; }
     }
 }
